fe9b3ae928aa23967b91a464acca88cd2908b2a4
Muhammad Inam Aslam <inam.aslam003@gmail.com>
2025-05-18 23:29:12 +0500

    make database connection

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log(error);
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server is running on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("Error: ", error);
+//   }
+// })();
