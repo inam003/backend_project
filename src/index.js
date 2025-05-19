c84cc148b485bc8ecb4673c5f1e145eebb3ddb51
Muhammad Inam Aslam <inam.aslam003@gmail.com>
2025-05-19 10:13:27 +0500

    utils files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import app from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
@@ -5,7 +6,19 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+
+    app.on("error", (error) => {
+      console.log(error);
+    });
+  })
+  .catch((error) => {
+    console.log("Error: ", error);
+  });
 
 // (async () => {
 //   try {
